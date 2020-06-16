e0e659b00ab7ac56ef022dace2a06f1951ae9ff4
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-06-16 17:34:12 +0300

    fix menu

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-//
-// // Get the header
-// var header = document.getElementById("header");
-//
-// // Get the offset position of the navbar
-// var sticky = header.offsetTop;
-//
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else if (window.pageXOffset > sticky) {
-//     header.classList.add("sticky");
-//   }
-//   else {
-//     header.classList.remove("sticky");
-//   }
-// }
-
-document.getElementById('menu').onclick = e => {
-  e.target.style.display = 'none';
-  document.getElementById('menu_open').style.display = 'block';
-  document.getElementById('map').style.display = 'block';
-  document.getElementById('about').style.display = 'block';
-  document.getElementById('encyclopedia').style.display = 'block';
-  document.getElementById('shop').style.display = 'block';
-  // document.querySelector('menu_text').style.display = 'block';
-}
-
-document.getElementById('menu_open').onclick = e => {
-  e.target.style.display = 'none';
-  document.querySelectorAll('menu_text').style.display = 'none';
-  document.getElementById('menu').style.display = 'block';
-}
-
-document.getElementById('map').onclick = e => {
-  e.target.style.textDecorationStyle = 'line-through';
+document.getElementById('header__nav__burger-btn').onclick = e => {
+  e.target.classList.toggle('is-open');
+  document.querySelector('.header__nav__menu').classList.toggle('is-open');
 }
