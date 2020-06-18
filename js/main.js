7d9f75f84cc114f5b9f23337c7e04df6a1baef90
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-06-18 05:41:19 +0300

    shop

--- js/main.js
+++ js/main.js
@@ -12,3 +12,13 @@ document.getElementById('poster_hover').onmouseleave = e => {
   e.target.style.display = 'none';
   document.getElementById('poster').style.display = 'block';
 }
+
+document.getElementById('guide').onmouseenter = e => {
+  e.target.style.display = 'none';
+  document.getElementById('guide_hover').style.display = 'block';
+}
+
+document.getElementById('guide_hover').onmouseleave = e => {
+  e.target.style.display = 'none';
+  document.getElementById('guide').style.display = 'block';
+}
