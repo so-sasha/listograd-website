8b0284f3684abee37e2c2f8b2858dea05ca32641
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-06-18 11:17:19 +0300

    map text

--- js/index.js
+++ js/index.js
@@ -10,3 +10,27 @@ document.getElementById('plane').onmouseenter = function() {
 document.getElementById('plane_text').onmouseleave = e => {
   e.target.style.display = 'none';
 }
+
+document.getElementById('monstera').onmouseenter = function() {
+  document.getElementById('monstera_text').style.display = 'block';
+}
+
+document.getElementById('monstera_text').onmouseleave = e => {
+  e.target.style.display = 'none';
+}
+
+document.getElementById('fern_shrub').onmouseenter = function() {
+  document.getElementById('fern_shrub_text').style.display = 'block';
+}
+
+document.getElementById('fern_shrub_text').onmouseleave = e => {
+  e.target.style.display = 'none';
+}
+
+document.getElementById('leaf_phisalis').onmouseenter = function() {
+  document.getElementById('leaf_phisalis_text').style.display = 'block';
+}
+
+document.getElementById('leaf_phisalis_text').onmouseleave = e => {
+  e.target.style.display = 'none';
+}
