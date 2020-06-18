5f393cfeee882dcb001cdc9379771e922e06d36f
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-06-18 04:28:13 +0300

    shop

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ document.getElementById('header__nav__burger-btn').onclick = e => {
   e.target.classList.toggle('is-open');
   document.querySelector('.header__nav__menu').classList.toggle('is-open');
 }
+
+document.getElementById('poster').onmouseenter = e => {
+  e.target.style.display = 'none';
+  document.getElementById('poster_hover').style.display = 'block';
+}
+
+document.getElementById('poster_hover').onmouseleave = e => {
+  e.target.style.display = 'none';
+  document.getElementById('poster').style.display = 'block';
+}
